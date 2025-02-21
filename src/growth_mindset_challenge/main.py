d1e205c3d59746d0aef8fbda911f93cf0bf65481
Shiraz Ali <shirazkk8@gmail.com>
2025-02-21 12:12:21 +0500

    fix_deployement_issue

--- src/growth_mindset_challenge/main.py
+++ src/growth_mindset_challenge/main.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from crewai.flow.flow import Flow, start, listen, router
 from litellm import completion
 import os 
